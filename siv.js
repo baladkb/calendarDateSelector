9fc8d37a2bf09bef3427d9993c0299b4e4f46cff
balakrishanan <balakrishanan.k@spritle.com>
2016-03-10 16:02:19 +0530

    siv.js lib modification and Demo file change

--- siv.js
+++ siv.js
@@ -1,115 +1,133 @@
 //Javascript file
 
-function showCalendar(idCalendar, isCalledFromOverlay) {
-	var sCalledFor;
-	if (isCalledFromOverlay) {
-		sCalledFor = 'calendar_date_selector_field';
-	}
-	calendarComponent_Init(sCalledFor);
-	calendarComponent_Show(idCalendar, sCalledFor);
-}
+var DateSelector = {
+		init: function() {
+			console.log("init")
+		},
+		showCalendar: function (idCalendar, isCalledFromOverlay) {
+			this.sCalledFor;
+			if (isCalledFromOverlay) {
+				sCalledFor = 'calendar_date_selector_field';
+			}
+			calendarComponent_Init(sCalledFor);
+			calendarComponent_Show(idCalendar, sCalledFor);
+		},
 
-function updateInitial(sCont, sVal) {
-	if (sCont == 'A') {
-		document.getElementById('fromDateID').value = sVal.value;
-	} else {
-		document.getElementById('idToDate').value = sVal.value;
-	}
-}
+		updateInitial: function (sCont, sVal) {
+			if (sCont == 'A') {
+				document.getElementById('fromDateID').value = sVal.value;
+			} else {
+				document.getElementById('idToDate').value = sVal.value;
+			}
+		},
 
-function siv_currentDate() {
-	var dateObj = new Date();
-	return informe_formatDate(dateObj);
-}
+		siv_currentDate: function () {
+			dateObj = new Date();
+			currentD = DateSelector.siv_formatDate(dateObj);
+			return currentD;
+		},
 
-function informe_formatDate(date) {
-	var month = date.getUTCMonth() + 1; // months from 1-12
-	var day = date.getUTCDate();
-	var year = date.getUTCFullYear();
-	return day + "/" + month + "/" + year
-}
+		siv_formatDate: function (date) {
+			month = date.getUTCMonth() + 1;
+			day = date.getUTCDate();
+			year = date.getUTCFullYear();
+			ddmmyyy = day + "/" + month + "/" + year;
+			return ddmmyyy;
+		},
 
-function informe_YesterDay() {
-	var d = new Date();
-	yday = d.setDate(d.getDate() - 1);
-	return informe_formatDate(new Date(yday));
-}
+		siv_YesterDay: function () {
+			d = new Date();
+			yday = d.setDate(d.getDate() - 1);
+			ydate = DateSelector.siv_formatDate(new Date(yday));
+			return ydate;
+		},
 
-function LastSevenDay() {
-	var d = new Date();
-	yday = d.setDate(d.getDate() - 7);
-	return informe_formatDate(new Date(yday));
-}
+	
 
-function LastMonthStart() {
-	var date = new Date(), y = date.getFullYear(), m = date.getMonth() - 1;
-	var firstDay = new Date(y, m, 1);
-	return informe_formatDate(firstDay);
-}
+			LastSevenDay: function () {
+			d = new Date();
+			yday = d.setDate(d.getDate() - 7);
+			ldate = DateSelector.siv_formatDate(new Date(yday));
+			return ldate;
+		},
 
-function LastMonthEnd() {
-	date = new Date(), y = date.getFullYear(), m = date.getMonth() - 1;
-	var lastDay = new Date(y, m + 1, 0);
-	return informe_formatDate(lastDay);
-}
+		LastMonthStart:  function () {
+			date = new Date(), y = date.getFullYear(), m = date.getMonth() - 1;
+			firstDay = new Date(y, m, 1);
+			lmsdate = DateSelector.siv_formatDate(firstDay);
+			return lmsdate;
+		},
 
-function LastWeekStart() {
-	var d = new Date();
-	var from = d.setTime(d.getTime() - 6 * 24 * 60 * 60 * 1000);
-	return informe_formatDate(new Date(from));
-}
+		LastMonthEnd: function()  {
+			date = new Date(), y = date.getFullYear(), m = date.getMonth() - 1;
+			lastDay = new Date(y, m + 1, 0);
+			lmedate = DateSelector.siv_formatDate(lastDay);
+			return lmedate;  
+		},
 
-function LatWeekEnd() {
-	var d = new Date()
-	var to = d.setTime(d.getTime() - (d.getDay() ? d.getDay() : 7) * 24 * 60
-			* 60 * 1000);
-	return informe_formatDate(new Date(to));
-}
-function Last30Days() {
-	var d = new Date();
-	yday = d.setDate(d.getDate() - 30);
-	return informe_formatDate(new Date(yday));
-}
+		LastWeekStart: function () {
+			d = new Date();
+			from = d.setTime(d.getTime() - 6 * 24 * 60 * 60 * 1000);
+			lwsdate = DateSelector.siv_formatDate(new Date(from));
+			return lwsdate;
+		},
 
-function HideDatePickerIcon() {
-	document.getElementById("calenderStartId").style.display = "none";
-	document.getElementById("calenderEndId").style.display = "none";
-}
+		LatWeekEnd: function () {
+			d = new Date()
+			to = d.setTime(d.getTime() - (d.getDay() ? d.getDay() : 7) * 24 * 60
+					* 60 * 1000);
+			lwedate = DateSelector.siv_formatDate(new Date(to));
+			return lwedate;
+		},
+		Last30Days: function () {
+			d = new Date();
+			yday = d.setDate(d.getDate() - 30);
+			lddate = DateSelector.siv_formatDate(new Date(yday));
+			return lddate;  
+		},
 
-function ShoweDatePickerIcon() {
-	document.getElementById("calenderStartId").style.display = "";
-	document.getElementById("calenderEndId").style.display = "";
-}
+		HideDatePickerIcon: function () {
+			console.log("Hide");
+			document.getElementById("calenderStartId").style.visibility = "hidden";
+			document.getElementById("calenderEndId").style.visibility = "hidden";
+		},
 
-function setCalenderDate(from, to) {
-	document.getElementById("calendar_date_selector_field_instance0").value = from;
-	document.getElementById("calendar_date_selector_field_instance1").value = to;
-}
+		ShoweDatePickerIcon: function () {
+			console.log("Open");
+			document.getElementById("calenderStartId").style.visibility = "";
+			document.getElementById("calenderEndId").style.visibility = "";
+		},
 
-function informe_selectDate() {
-	var dateSelected = document.getElementById('selectDatesId').value;
+		setCalenderDate: function (from, to) {
+			document.getElementById("calendar_date_selector_field_instance0").value = from;
+			document.getElementById("calendar_date_selector_field_instance1").value = to;
+		},
 
-	HideDatePickerIcon();
-	switch (dateSelected) {
-	case "0":
-		setCalenderDate(siv_currentDate(), siv_currentDate());
-		break;
-	case "1":
-		setCalenderDate(informe_YesterDay(), informe_YesterDay());
-		break;
-	case "2":
-		setCalenderDate(LastWeekStart(), LatWeekEnd());
-		break;
-	case "3":
-		setCalenderDate(LastMonthStart(), LastMonthEnd());
-		break;
-	case "4":
-		setCalenderDate(siv_currentDate(), LastSevenDay());
-		break;
-	case "5":
-		setCalenderDate(siv_currentDate(), Last30Days());
-		break;
-	default:
-		ShoweDatePickerIcon();
-	}
+		informe_selectDate: function () {
+			dateSelected = document.getElementById('selectDatesId').value;
+
+			DateSelector.HideDatePickerIcon();
+			switch (dateSelected) {
+			case "0":
+				DateSelector.setCalenderDate(DateSelector.siv_currentDate(), DateSelector.siv_currentDate());
+				break;
+			case "1":
+				DateSelector.setCalenderDate(DateSelector.siv_YesterDay(), DateSelector.siv_YesterDay());
+				break;
+			case "2":
+				DateSelector.setCalenderDate(DateSelector.LastWeekStart(), DateSelector.LatWeekEnd());
+				break;
+			case "3":
+				DateSelector.setCalenderDate(DateSelector.LastMonthStart(), DateSelector.LastMonthEnd());
+				break;
+			case "4":
+				DateSelector.setCalenderDate(DateSelector.siv_currentDate(), DateSelector.LastSevenDay());
+				break;
+			case "5":
+				DateSelector.setCalenderDate(DateSelector.siv_currentDate(), DateSelector.Last30Days());
+				break;
+			default:
+				DateSelector.ShoweDatePickerIcon();
+			}
+		}
 }
